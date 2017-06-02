7bf43d63956c49033ea87473f0d9f721905bd881
pyaly <991526442@qq.com>
2017-06-02 17:49:25 +0800

    Responsive Admin Web App

---
+++ flatify/client/app/app.module.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+    angular.module('app', [
+         'ngRoute'
+        ,'ngAnimate'
+        ,'ngAria'
+
+        // 
+        ,'ui.bootstrap'
+        ,'easypiechart'
+        ,'mgo-angular-wizard'
+        ,'textAngular'
+        ,'angular-loading-bar'
+        ,'duScroll'
+
+        // 
+        ,'app.layout'
+        ,'app.i18n'
+        ,'app.ui'
+        ,'app.ui.form'
+        ,'app.table'
+        ,'app.chart'
+    ])
+})(); 
+
